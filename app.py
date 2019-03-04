fc01b519b0e10f9b4586ca3990770b19ab606b53
Eric Briscoe <eric0briscoe@gmail.com>
2019-03-03 20:37:17 -0600

    add get_magnet_links

--- app.py
+++ app.py
@@ -30,22 +30,56 @@ def sync():
             os.mkdir(os.path.join(all_show_dir, show))
         show_dir = os.path.join(all_show_dir, show)
         episode_files = os.listdir(show_dir)
+        parsed_files = []
+        for file in episode_files:
+            n = re.findall(" [0-9]+[.]*[0-9]* ")
+            parsed_files += n
         show_url = config["settings"]["base_url"] + "/" + show
+        episodes = get_episodes(show_url)
+        episodes_to_fetch = []
+        for e in episodes:
+            if e not in parsed_files:
+                episodes_to_fetch.append(e)
+        links_to_add = get_magnet_links(show_url, episodes_to_fetch)
 
-        break
+
+def get_magnet_links(show_url, episodes):
+    wait_time = 0.2
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    chromedriver_path = config["settings"]["chromedriver_path"]
+    driver = webdriver.Chrome(
+        chromedriver_path
+    )  # Optional argument, if not specified will search path.
+    driver.get(show_url)
+    while True:
+        try:
+            show_more = driver.find_element_by_link_text("Show more ▼")
+            show_more.click()
+            time.sleep(wait_time)
+        except:
+            break
+
+    episode_buttons = driver.find_elements_by_class_name("rls-label")
+    for button in episode_buttons:
+        button.click()
+        time.sleep(wait_time)
+    magnets = []
+    episode_container = driver.find_element_by_class_name("episode-container")
+    for ep in episodes:
+        try:
+            link_container = driver.find_element_by_id(str(ep) + "-1080p")
+            link_element = link_container.find_element_by_link_text("Magnet")
+            magnets.append(link_element.get_attribute("href"))
+        except:
+            print('Couldn\'t find episode ' + str(ep))
+    print(magnets)
+    return magnets
+    driver.close()
 
 
 def get_episodes(show_url):
-    from urllib.request import Request, urlopen
-
-    req = Request(show_url, headers={"User-Agent": "Mozilla/5.0"})
-
-    html = urlopen(req).read()
-    print(html)
-
-
-def selenium_get_episodes(show_url):
-    wait_time = .2
+    wait_time = 0.2
     config = configparser.ConfigParser()
     config.read("config.ini")
     chromedriver_path = config["settings"]["chromedriver_path"]
@@ -54,9 +88,9 @@ def selenium_get_episodes(show_url):
     )  # Optional argument, if not specified will search path.
     driver.get(show_url)
     # Check for show more, expand if found
-    while(True):
+    while True:
         try:
-            show_more = driver.find_element_by_link_text('Show more ▼')
+            show_more = driver.find_element_by_link_text("Show more ▼")
             show_more.click()
             time.sleep(wait_time)
         except:
@@ -64,9 +98,10 @@ def selenium_get_episodes(show_url):
 
     episode_container = driver.find_element_by_class_name("episode-container")
     print(episode_container.text)
-    episodes = re.findall('( [0-9]+[.]*[0-9]* )\w+', episode_container.text)
-    print(episodes)
+    episodes = re.findall("( [0-9]+[.]*[0-9]* )\w+", episode_container.text)
+    episodes = [e.replace(" ", "") for e in episodes]
     driver.close()
+    return episodes
 
 
 def set_config():
@@ -88,4 +123,5 @@ def set_config():
 if __name__ == "__main__":
     set_config()
     # sync()
-    selenium_get_episodes("http://horriblesubs.info/shows/goblin-slayer/")
+    # get_episodes("http://horriblesubs.info/shows/goblin-slayer/")
+    get_magnet_links("http://horriblesubs.info/shows/goblin-slayer/", ['01', '02'])
